a3f4afbf5a9e1a2440097ceacba92c703e3b0a1f
paeng1986 <67446318+paeng1986@users.noreply.github.com>
2022-08-26 10:01:15 +0800

    remove console and added async await to create task

--- resources/js/api/Tasks.tsx
+++ resources/js/api/Tasks.tsx
@@ -5,14 +5,10 @@ const Headers: any = {
         'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]')?.getAttribute("content")
     }
 }
-
-console.log(Headers);
-
-
 const BASE_URL = 'http://127.0.0.1:8000/api/tasks';
 
 export const
-    Create = (task: any) => { return Axios.post(BASE_URL, task, Headers); },
+    Create = async (task: any) => { return await Axios.post(BASE_URL, task, Headers); },
     Read = async () => { return await Axios.get(BASE_URL); },
     Update = async (task: any) => { return await Axios.put(BASE_URL + `/${task.id}`, task) },
     UpdateStatus = async (id: number, status: string) => { return await Axios.put(BASE_URL + `/${id}`, { id, status }); },
